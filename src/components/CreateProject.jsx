e4ac725245e95d65766640cd56b5d5d75fcbe08f
ninehundred <daniel.odowd@arup.com>
2021-11-02 11:50:17 +0000

    add post request api to CreateProject

--- src/components/CreateProject.jsx
+++ src/components/CreateProject.jsx
@@ -1,15 +1,37 @@
-import react from 'react';
-import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import '../styles/proj-input-form.css'
+import { ErrorMessage } from './ErrorMessage';
+import  { Redirect } from 'react-router-dom'
+import { postProject } from '../utils/api';
 
 
 
 export const CreateProject = () => {
 
     const [displayForm, setDisplayForm] = useState(false);
+    const [formInput, setFormInput] = useState({username: ''});
+    const [error, setError] = useState(false);
+    
 
+    const handleChange = (event) => {
+      const name = event.target.name;
+      const value = event.target.value;
+      setFormInput(values => ({...values, [name]: value }))
+    }
 
+    const LoginSubmit = (event) => {
+      event.preventDefault();
+      postProject( formInput )
+      // TODO - at this point we want to redirect to the project page
+      // .then(projectData => {
+      //   //TODO - want tell user project successful or not.
+      //   // TODO - tell user here if the error was part of the form
+          //    otherwise we can redirect them to the error page.
+      // })
+    }
+
+     
+    // TODO - add request to server to send info
     const handleClick = (event) => {
       event.preventDefault();
       setDisplayForm((curDisplay) => {
@@ -30,13 +52,31 @@ export const CreateProject = () => {
        </section>
 
        {displayForm && 
-        <form className='project_input_form'>
-          <label for="projTitle" className='proj_create_label'>project title:</label>
-          <input type="text" id="projTitle" name="projTitle" className='proj_create_input'/>
-          <label for="jobNo" className='proj_create_label'>job number:</label>
-          <input type="text" id="jobNo" name="jobNo" className='proj_create_input'/>
-          <label for="projType" className='proj_create_label'>project type:</label>
-          <input type="text" id="projTpe" name="projType" className='proj_create_input'/>
+        <form className='project_input_form' onSubmit={LoginSubmit}>
+          <label htmlFor="projTitle" className='proj_create_label'>project title:</label>
+          <input type="text" 
+                 id="projTitle" 
+                 name="projTitle" 
+                 className='proj_create_input'
+                 onChange={handleChange} 
+                 required/>
+                 { error ? <ErrorMessage error={error}/> : null } 
+          <label htmlFor="jobNo" className='proj_create_label'>job number:</label>
+          <input type="text" 
+                 id="jobNo" 
+                 name="jobNo" 
+                 className='proj_create_input'
+                 onChange={handleChange} 
+                 required/>
+                 { error ? <ErrorMessage error={error}/> : null } 
+          <label htmlFor="projType" className='proj_create_label'>project type:</label>
+          <input type="text" 
+                 id="projType" 
+                 name="projType" 
+                 className='proj_create_input'
+                 onChange={handleChange} 
+                 required/>
+                 { error ? <ErrorMessage error={error}/> : null } 
           <input type="submit" value="save" className='save_proj_btn'/>
         </form>
         }
---
+++ src/utils/api.js
@@ -0,0 +1,21 @@
+import axios from  'axios'
+import { AxiosResponse, AxiosError } from 'axios'
+
+
+// TODO - need to change this with our actual URL
+const listApi = axios.create({baseURL:`https://be-nc-news-testing.herokuapp.com/api`})
+
+export const postProject = (projectInfo) => {
+  // TODO - may have to edit the keys on this object to match API keys.
+  const projectObject = {"project_name" : projectInfo.projTitle,
+                         "project_type" : projectInfo.projType,
+                          "job_number" : projectInfo.jobNo}
+  console.log("project info sent through>>>>\n" ,projectObject)
+  //return listApi.post(`ADD REST OF URL HERE`, projectObject)
+  // .then( ( {data} ) => {
+  // })
+  // .catch(err => {
+  //   //console.dir(err)
+  //   //TODO - should redirect to error page here
+  // })
+}
